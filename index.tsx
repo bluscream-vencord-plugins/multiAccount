d495c415db6157392037caec0b6a0ad8f35129b5
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:25 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -30,19 +30,8 @@ import { Logger } from "@utils/Logger";
 import { settings } from "./settings";
 // endregion Imports
 
-// region PluginInfo
-export const pluginInfo = {
-    id: "multiAccount",
-    name: "MultiAccount",
-    description: "Allows using multiple Discord accounts in one instance by merging DMs and servers",
-    color: "#7289da",
-    authors: [
-        Devs.D3SOX,
-        { name: "Bluscream", id: 467777925790564352n },
-        { name: "Assistant", id: 0n }
-    ],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 
 // region Variables
 const logger = new Logger(pluginInfo.id, pluginInfo.color);
---
+++ info.ts
@@ -0,0 +1,13 @@
+import { Devs, EquicordDevs } from "@utils/constants";
+
+export const pluginInfo = {
+    id: "multiAccount",
+    name: "MultiAccount",
+    description: "Allows using multiple Discord accounts in one instance by merging DMs and servers",
+    color: "#7289da",
+    authors: [
+        Devs.D3SOX,
+        { name: "Bluscream", id: 467777925790564352n },
+        { name: "Assistant", id: 0n }
+    ],
+};
